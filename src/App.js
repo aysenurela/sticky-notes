c503e41eb2354fd3c441de884f99e3719b171687
aysenur.ela <aysenur.ela@solvoyo.com>
2019-07-16 18:39:28 +0300

    react-sticky-notes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './StickyNotes.scss'
+import _ from 'lodash'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class StickyNotes extends Component {
+  state = {
+    events: [
+      { title: 'event1', description: 'description1' },
+      { title: 'event2', description: 'description2' }
+    ]
+  }
+
+  onChangeDescription (event) {
+    const { events } = this.state
+
+    var _events = [...events]
+
+    let newDesc = event.target.value
+
+    let eventIndex = event.target.id
+
+    _events[eventIndex].description = newDesc
+    this.setState({
+      events: _events
+    })
+  }
+
+  onChangeTitle (event) {
+    const { events } = this.state
+
+    var _events = [...events]
+
+    let newTitle = event.target.value
+
+    let eventIndex = event.target.id
+
+    _events[eventIndex].title = newTitle
+    this.setState({
+      events: _events
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <ul className='unordered'>
+          {this.state.events.map((row, index) => {
+            return (
+              <li key={index} className='list'>
+                <div className='cover'>
+                  <input
+                    className='title'
+                    id={index}
+                    type='text'
+                    value={row.title}
+                    onChange={this.onChangeTitle.bind(this)}
+                  />
+                  <input
+                    className='p-desc'
+                    id={index}
+                    type='text'
+                    value={row.description}
+                    onChange={this.onChangeDescription.bind(this)}
+                  />
+                  <button
+                    className='delete-button'
+                    onClick={() => {
+                      console.log('delete-button')
+                    }}
+                  >
+                    x
+                  </button>
+                </div>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default StickyNotes
